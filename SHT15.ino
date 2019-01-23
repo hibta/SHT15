5e51626075857d7cc4859d817d81b4f36c8e831f
hibta <hibta0@gmail.com>
2019-01-23 20:07:23 +0900

    赤外線LEDの追加

--- SHT15.ino
+++ SHT15.ino
@@ -1,15 +1,14 @@
 #include <SHT1X.h>
+#include "IRremote.h"
+IRsend irsend;  //IRsendオブジェクトの生成
 
-int TIME = 30;  // 平均を計算する間隔(秒)
-float TEMP = 25;  // LEDが切り替わる気温の閾値(度)
-
+int TIME = 60;  // 室温を計算する間隔(秒)
+float TEMP = 30;  // LEDを点ける気温の閾値(度)
+int KHZ=38; // 信号の周波数は38[kHz]
 
 // 気温, 湿度を保存
 float tempC = 0;
 float humidity = 0;
-// 平均用
-float ave_tempC = 0;
-float ave_humidity = 0;
 
 //Create an instance of the SHT1X sensor
 SHT1x sht15(A4, A5);//Data, SCK
@@ -18,30 +17,23 @@ SHT1x sht15(A4, A5);//Data, SCK
 int power = A3;
 int gnd = A2;
 
-// LEDのピン
-int led_green = 12;
-int led_red = 13;
-
 void setup()
 {
   Serial.begin(9600); // Open serial connection to report values to host
 
   pinMode(power, OUTPUT);
   pinMode(gnd, OUTPUT);
-  pinMode(led_green, OUTPUT);
-  pinMode(led_red, OUTPUT);
 
   digitalWrite(power, HIGH);
   digitalWrite(gnd, LOW);
-  digitalWrite(led_green, HIGH);
-  digitalWrite(led_red, HIGH);
 }
 //-------------------------------------------------------------------------------------------
 void loop()
 {
-  calAverage(TIME);  // 指定された秒数毎に平均を計算
-  printOut();
-  setLED(TEMP);  // 気温が閾値以下だと赤LEDを点灯
+  readSensor();  // 室温を測定
+  //printOut();  // シリアルモニタに値を表示
+  sendLED();  // 気温が閾値以下だとLEDを点灯
+  delay(TIME*1000);  // TIME秒待機
 }
 //-------------------------------------------------------------------------------------------
 void readSensor()
@@ -54,44 +46,21 @@ void readSensor()
 void printOut()
 {
   Serial.print(" Temp = ");
-  Serial.print(ave_tempC);
+  Serial.print(tempC);
   Serial.println("C");
   Serial.print(" Humidity = ");
-  Serial.print(ave_humidity); 
+  Serial.print(humidity); 
   Serial.println("%");
 }
 //-------------------------------------------------------------------------------------------
-void calAverage(int num)    // num秒毎に平均を計算
+// 室温がTEMP度以下だとLEDを発光
+void sendLED()
 {
-  // 一時保存用
-  float tmp_tempC = 0;
-  float tmp_humidity = 0;
-
-  // 1秒毎に足し合わせ, 計num回計算
-  for (int i=0; i<num; i++)
+  if (tempC <= TEMP)
   {
-    readSensor();
-    tmp_tempC += tempC;
-    tmp_humidity += humidity;
-    delay(1000);
-  }
-  
-  // numで割って平均を保存
-  ave_tempC = tmp_tempC / num;
-  ave_humidity = tmp_humidity / num;
-}
-//-------------------------------------------------------------------------------------------
-// 室温がtemp度以下だと赤色, それ以外は緑色のLEDを光らせる
-void setLED(float temp)
-{
-  if (ave_tempC <= temp)
-  {
-    digitalWrite(led_green, LOW);
-    digitalWrite(led_red, HIGH);
-  }
-  else
-  {
-    digitalWrite(led_green, HIGH);
-    digitalWrite(led_red, LOW);
+    // 赤外線LEDの信号パターン
+    unsigned int ledON[] = {3500,1650, 500,350, 500,1250, 450,400, 450,400, 450,400, 450,400, 500,400, 450,400, 450,400, 450,400, 450,400, 450,400, 500,350, 500,1250, 450,400, 450,400, 450,400, 450,400, 500,400, 450,400, 450,400, 450,1250, 450,1250, 500,1250, 450,400, 450,400, 450,1250, 500,400, 450,400, 450,400, 450,400, 450,400, 450,400, 500,400, 450,400, 450,400, 450,400, 450,400, 450,400, 450,400, 500,400, 450,400, 450,400, 450,400, 450,400, 500,350, 500,400, 450,400, 450,400, 450,400, 450,400, 450,400, 500,350, 500,400, 450,400, 450,400, 450,400, 450,1250, 500,1250, 450,400, 450,400, 450,400, 450,400, 500,400, 450};  // PANASONIC 4004:7200000
+    // 赤外線LEDを点灯
+    irsend.sendRaw(ledON, sizeof(ledON)/sizeof(ledON[0]), KHZ);
   }
 }
